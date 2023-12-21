b64ee0f64c43efe48fc7cfb2a989954154db5cfa
qasimjaved <qasim.javed012@gmail.com>
2023-12-22 04:07:00 +0500

    Temp commit

--- toolkit/crawler/selenium.py
+++ toolkit/crawler/selenium.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
+#
 
 def launch_browser():
     driver = webdriver.Chrome()
