2984e760237c42b41e455b1c6a866cb5cd0f3508
qasimjaved <qasim.javed012@gmail.com>
2023-12-24 18:06:14 +0500

    Helpers added

---
+++ toolkit/file.py
@@ -0,0 +1,26 @@
+
+def write_list_to_text_file(file_path, data):
+    """
+    Write list to text file
+
+    :param file_path:
+    :param data:
+    :return:
+    """
+    with open(file_path, "w") as file:
+        file.writelines(data)
+
+def read_list_from_text_file(file_path) -> list:
+    """
+
+    Check if file exists then read list from text file
+
+    :param file_path:
+    :return:
+    """
+    if not os.path.exists(file_path):
+        return []
+
+    with open(file_path, "r") as file:
+        data = file.readlines()
+    return data
