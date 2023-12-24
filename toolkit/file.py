51cac1880fe7949535d8f4b71fa691d0dd41ae31
qasimjaved <qasim.javed012@gmail.com>
2023-12-24 18:10:59 +0500

    Helpers added

--- toolkit/file.py
+++ toolkit/file.py
@@ -2,14 +2,15 @@ import os
 
 def write_list_to_text_file(file_path, data):
     """
-    Write list to text file
+    Write list to text file in splitting elements to new line
 
     :param file_path:
     :param data:
     :return:
     """
     with open(file_path, "w") as file:
-        file.writelines(data)
+        file.write("\n".join(data))
+
 
 def read_list_from_text_file(file_path) -> list:
     """
